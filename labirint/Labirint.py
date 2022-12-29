73c602661d51ef4f9bf85f2962d34734ccc561df
EgorLz <95042247+EgorLz@users.noreply.github.com>
2022-12-29 15:11:29 +0300

    Update Labirint.py

--- labirint/Labirint.py
+++ labirint/Labirint.py
@@ -12,7 +12,16 @@ points = [[1, 2, 3],
 point_y = len(points)
 point_x = len(points[0])
 
-graf = [[2, 4], [1, 3, 5], [2, 9], [1, 5], [2, 4, 6], [3, 5, 9], [8], [7, 9], [6, 8]]
+graf = {1: [2, 4],
+        2: [1, 3, 5],
+        3: [2, 6],
+        4: [1, 5],
+        5: [2, 4, 6],
+        6: [3, 5, 9],
+        7: [8],
+        8: [7, 9],
+        9: [6, 8]}
+
 no_reb = [[4, 7], [5, 8]]
 
 screen = pygame.display.set_mode((point_x * 150, point_y * 150))
@@ -32,6 +41,50 @@ def draw():
             pygame.draw.line(screen, "black", [((end % point_x) - 1) * 150, 150 * (end // point_y)], [(end % point_x) * 150, 150 * (end // point_y)], 8)
 draw()
 
+#def way(x, y, x_end, y_end, graf):
+    #short = []
+    #counted = []
+    #min = []
+    #start_point = (x + 1) + (y * 3)
+    #now = start_point
+    #short.append(start_point)
+    #counted.append(start_point)
+    #end_point = (x_end + 1) + (y_end * 3)
+    #if end_point in graf[start_point - 1]:
+        #counted.append(end_point):
+        #return counted
+    #else:
+        #for i in range(len(graf[start_point - 1])):
+
+def dfs_paths(graph, x_end, y_end):
+    way = [(x_end, [x_end])]  # (vertex, path)
+    while way:
+        (vertex, path) = way.pop()
+        for next in set(graph[vertex]) - set(path):
+            if next == y_end:
+                yield path + [next]
+            else:
+                way.append((next, path + [next]))
+
+def printp(path):
+    for f in path:
+        if f % 3 == 0:
+            point_x = 2
+            point_y = f // 3 - 1
+        else:
+            point_x = f % 3 - 1
+            point_y = f // 3
+        pygame.draw.rect(screen, "yellow", (150 * point_x + 5, 150 * point_y + 5, 142, 142))
+
+def robot(x, y):
+    pygame.draw.circle(
+                    screen, "red", (x * 150 + 75, y * 150 + 75), 70)
+
+def roboty(x, y):
+    pygame.draw.circle(
+                    screen, "yellow", (x * 150 + 75, y * 150 + 75), 70)
+
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -45,5 +98,26 @@ while True:
                 pos = [x * 150 + 75, y * 150 + 75]
                 pygame.draw.circle(
                     screen, "red", pos, 70)
+                pygame.display.update()
+            if event.button == 2:
+                draw()
+                pygame.display.update()
+            if event.button == 3:
+                x_end = event.pos[0] // 150
+                y_end = event.pos[1] // 150
+                pygame.display.update()
+
+                paths = list(dfs_paths(graf, points[y][x], points[y_end][x_end]))
+
+                paths2 = []
+                for i in paths:
+                    paths2.append(len(i))
+                path = paths[paths2.index(min(paths2))]
+
+                draw()
+                roboty(x, y)
+                printp(path)
+                robot(x_end, y_end)
+                x, y = x_end, y_end
                 pygame.display.update()
     pygame.display.flip()
